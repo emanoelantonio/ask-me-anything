fa3ef442606ae0977a5ec2cc8b896164004b2e1a
emanoelantonio <dev.emanoel@protonmail.com>
2024-08-08 17:53:01 -0300

    start: init base setup

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CreateRoom } from "./pages/CreateRoom";
+import Room from "./pages/Room";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CreateRoom />
+  },
+  {
+    path: '/room/:roomId',
+    element: <Room />
+  }
+])
+
+export function App() {
+  return <RouterProvider router={router} />
+}
---
+++ src/pages/CreateRoom.tsx
@@ -0,0 +1,6 @@
+
+export function CreateRoom() {
+  return (
+    <h1>CreateRoom</h1>
+  )
+}
