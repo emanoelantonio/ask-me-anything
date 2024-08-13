370d11c2729c7c4ce3be883d6346b137fce480fc
emanoelantonio <dev.emanoel@protonmail.com>
2024-08-13 17:13:50 -0300

    feat: add sonner, alert component.

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 import { CreateRoom } from "./pages/CreateRoom";
-import Room from "./pages/Room";
+import { Room } from "./pages/Room";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <CreateRoom />
+    path: "/",
+    element: <CreateRoom />,
   },
   {
-    path: '/room/:roomId',
-    element: <Room />
-  }
-])
+    path: "/room/:roomId",
+    element: <Room />,
+  },
+]);
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </>
+  );
 }
