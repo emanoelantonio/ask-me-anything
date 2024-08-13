08cc858ec43c5c2e149f141a2dcff87842724345
emanoelantonio <dev.emanoel@protonmail.com>
2024-08-13 17:10:17 -0300

    chore: change icon, package config and eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended
+  ],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
+  parser: '@typescript-eslint/parser',
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
